5491b1c185296e0794624687f9e44863ae9dac3e
xkrek <gdima0709@gmail.com>
2025-06-18 19:12:03 +0300

    v 1.1 add list computers

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "computer.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 5;
+	computer* arr = new computer[n]{
+		{"Asus", 8, 15'000},
+		{"Dell", 16, 25'000},
+		{"HP", 32, 50'000},
+		{"Lenovo", 16, 30'000},
+		{"Acer", 8, 20'000}
+	};
 }
